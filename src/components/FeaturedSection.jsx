05a1ce248ebb8729bd4bafec0acd21915e7c2fe7
Akite Donald <donaldakite27@gmail.com>
2024-08-25 15:04:03 +0000

    Add change

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -15,12 +15,7 @@ const services = [
       "Our brand management process integrates together a perfect mix of modules including defining the brand, positioning the brand, and delivering the brand value constantly.",
     icon: <FaRegLightbulb />
   },
-  {
-    title: "Consultancy",
-    description:
-      "We offer support services by coordinating with the Ghana Immigration service in cases of emergencies. Get visa application assistance for the UK, USA, Dubai, Canada, Schengen and Sao Tome.",
-    icon: <FaHeadset />
-  },
+
   {
     title: "Digital Marketing",
     description:
@@ -32,6 +27,12 @@ const services = [
     description:
       "PedStar manages your office setup deployments following best industry protocols that will get your office space professionally and running properly. We will procure and deliver all your office needs.",
     icon: <FaBriefcase />
+  },
+  {
+    title: "Support services and consultancy",
+    description:
+      "we offer assistance with work permits, residence permits, immigration and settlement information, and visa on arrival. Our visa services cover destinations including the UK, USA, Dubai, Canada, Schengen, Asia, Sao Tomè, and other locations.",
+    icon: <FaHeadset />
   },
   {
     title: "Export & Import",
@@ -64,14 +65,14 @@ const FeaturedSection = () => {
           {services.map((service, index) => (
             <div
               key={index}
-              className="relative w-full md:w-80 p-6 m-4 rounded-xl overflow-hidden bg-white border border-gray-300 text-center"
+              className="relative h-full w-full md:w-80 p-6 m-4 rounded-xl overflow-hidden bg-white border border-gray-300 text-center"
             >
               <img
                 src={require("../images/blue-dot-pattern.webp")}
                 alt={`Item ${index + 1}`}
                 className="absolute top-0 left-0 w-32 h-32 object-cover opacity-30 transform rotate-45 -translate-x-1/2 -translate-y-1/2"
               />
-              <div className="absolute top-2 right-2 p-2 z-10 text-white bg-[#093376] text-sm rounded-full">
+              <div className="absolute top-2 right-2 p-2 z-10 text-white bg-[#093376] text-lg rounded-full">
                 {service.icon}
               </div>
               <h3 className="text-lg font-semibold mb-2 mt-6 text-[#093376]">
