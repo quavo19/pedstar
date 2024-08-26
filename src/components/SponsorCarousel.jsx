521b0cc2d7c807fa5f182daa3af0f93f59657b77
Akite Donald <donaldakite27@gmail.com>
2024-08-26 15:14:32 +0000

    Center logos

--- src/components/SponsorCarousel.jsx
+++ src/components/SponsorCarousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 
 const sponsors = [
-  { src: require("../images/brands/BBautomobile.png"), alt: "Sponsor 1" },
+  { src: require("../images/brands/BBautomobile.png"), alt: "Sponsor" },
   { src: require("../images/brands/addidas.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/alitalia.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/chgroup.png"), alt: "Sponsor 1" },
@@ -17,19 +17,17 @@ const sponsors = [
   { src: require("../images/brands/melcom.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/nike.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/sahara.png"), alt: "Sponsor 1" },
+  { src: require("../images/brands/sheildmc.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/stanbic.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/union.png"), alt: "Sponsor 1" },
   { src: require("../images/brands/upsa.png"), alt: "Sponsor 1" },
-  { src: require("../images/brands/usaid.png"), alt: "Sponsor 1" }
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" },
-  //   { src: require("../images/brands"), alt: "Sponsor 1" }
+  { src: require("../images/brands/usaid.png"), alt: "Sponsor 1" },
+  { src: require("../images/brands/airfrance.png"), alt: "Sponsor 1" },
+  { src: require("../images/brands/klm.png"), alt: "Sponsor 1" },
+  {
+    src: require("../images/brands/pedessence-logo-clear-tiny (1).png"),
+    alt: "Sponsor 1"
+  }
 ];
 
 const SponsorCarousel = () => {
@@ -37,11 +35,13 @@ const SponsorCarousel = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 6, // Show 6 items at a time
-    slidesToScroll: 1,
+    slidesToShow: 5, // Show 5 items at a time on medium screens and larger
+    slidesToScroll: 5,
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false, // Hide navigation arrows
+    centerMode: false, // Enable center mode
+    centerPadding: "0", // Remove default padding in center mode
     responsive: [
       {
         breakpoint: 1024,
@@ -60,13 +60,13 @@ const SponsorCarousel = () => {
 
   return (
     <div className="md:px-24 px-6 pt-9">
-      <Slider {...settings} className="flex space-x-4">
+      <Slider {...settings} className="flex justify-center items-center">
         {sponsors.map((sponsor, index) => (
           <div key={index} className="flex items-center justify-center p-4">
             <img
               src={sponsor.src}
-              alt={sponsor.alt}
-              className="w-full h-auto"
+              alt={sponsor.alt + index}
+              className="max-w-full h-auto"
             />
           </div>
         ))}
